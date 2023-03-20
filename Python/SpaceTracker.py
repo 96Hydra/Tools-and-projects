6c8e6af1fb10a6492e6934b961682fc792b059a4
Patrick L <97918012+96Hydra@users.noreply.github.com>
2023-03-20 15:52:31 -0500

    Fun little Astronaut tracker

--- Python/SpaceTracker.py
+++ Python/SpaceTracker.py
@@ -4,4 +4,4 @@ details = ISS_Info.iss_people_in_space()
 print("There are currently {} people in space".format(details['number']))
 
 for p in details['people']:
-    print ("Name: {} (Craft: {}".format(p['name'],p['craft']))
+    print ("Name: {} (Craft: {})".format(p['name'],p['craft']))
